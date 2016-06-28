dd822e57a7f893f717f9c2b59139dbc345564b19
Matt Rose <rose.matt@gmail.com>
2016-06-28 14:01:40 -0400

    Remove some old ie stuff

--- gemini.gmaps.js
+++ gemini.gmaps.js
@@ -240,27 +240,6 @@ A Gemini plugin to easily interact with the Google Maps API
         var zoom = P.map.getZoom();
         P.map.setZoom(Math.min(zoom, P.mapOptions.zoom));
       });
-    },
-
-    /**
-     * Get the current version of IE
-     *
-     * @private
-     * @method
-     * @name gemini.gmaps#_getIEVersion
-    **/
-    _getIEVersion: function(){
-      // Returns the version of Windows Internet Explorer or a -1
-      // (indicating the use of another browser).
-      var rv = -1; // Return value assumes failure.
-      if (navigator.appName == 'Microsoft Internet Explorer')
-      {
-        var ua = navigator.userAgent;
-        var re  = new RegExp("MSIE ([0-9]{1,}[\\.0-9]{0,})");
-        if (re.exec(ua) !== null)
-          rv = parseFloat( RegExp.$1 );
-      }
-      return rv;
     }
   });
 
